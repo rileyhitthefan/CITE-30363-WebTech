ff8bed375ba2e494f14e8fba32350c94a050a7a0
riley phan <riley.phan@tcu.edu>
2025-02-22 13:32:59 -0600

    add hw1

---
+++ HW1/Problem5/constants.js
@@ -0,0 +1,38 @@
+const RESULTS_MAP = {
+  'blepping': {
+                title: 'Wonderful character judge',
+                contents: "Your adorable look gives a false representation of the sharp hunting dog inside, and you have an amazing nose, both for tracking down human's wants and for discovering delicious food. You snicker when you're befuddled and tilt your head when things are clever. In works of fiction, your preferred characters are the ones who have complex stories, and you appreciate winding subplots. In any case, you like short sentences. You have for the longest time been itching to begin a nursery. Shockingly, plants are not as straightforward as people, and the last time you dealt with a blossom, it didn't turn out so well. Be that as it may, you take excellent consideration of your companions, and they are fortunate to have you.",
+              },
+  'burger': {
+              title: 'Steadfast and determined',
+              contents: "You are biting on a cloth toy, and it will take a dozen tug-of-war specialists to wrest it from your mouth. Similarly, you are more likely to influence the people around you than be influenced by them. The quality of your work is very important to you. You are a visual thinker, and you are good at finding analogies between two disparate topics. You also enjoy sharing jokes and memes with your friends. You are picky about what you eat, but you will also try every new dish once. You believe that contradiction is the start of your soul. Sometimes you don't realize it, but many people look up to you.",
+            },
+  'cart': {
+              title: 'Perfectionist with a soft side',
+              contents: "You are warm and an expert of making everyone's day better. However, you like the idea of being a rebel (secretly), but most of the rebelling stuff just stays in your head. You like drawing flowcharts. You get a rush from finishing everything you have to do for the day, and when you are focused, it takes a lot to break your concentration. Your favorite color has changed many times over the course of your life. It is important to you to have creative outlets and personal independence. Even though much of society values “not trying too hard,” you actually really enjoy people who try very hard. You are highly driven, which is often inspiring to others.",
+            },
+  'dopey': {
+              title: 'Silly with a reflective side',
+              contents: "Woof Woof! JUST Kidding! Of course you can speak English. You like swiming, jogging, climbing, walking, pushing up, strolling and riding unicycles. Just Kidding! Unicycles are not for dogs! Your MSN username was \"L0Lin_jail_out_soon\". Sometimes, you add a smiley face next to your credit card signature. Sometimes you tell people to look \"under there\". You secretly wear a gold chain with a boxing gloves charm and a dog-tag with the name \"Rockman\". However, when you are alone, you ponder the increasing commercialization of human creative capability and enjoy reciting Marx while walking in the forest. Your Candy Crush Saga password is the hexadecimal representation of the Chinese character for \"strength.\"",
+            },
+  'happy': {
+              title: 'A positive force in the world',
+              contents: "You are optimistic and are confident in your ability to change something for the better, whether that is a group, a process, or yourself. It is difficult to label you as an introvert or an extrovert. You have many books that you have been meaning to read, and next week you will start one. When you wake up in the morning, It is hard for you to remember what you dreamed about. You like the sound of rain but do not enjoy getting wet. You are more fearless than the average person, especially in terms of sharing who you are. At the same time, you tend to be very aware of how your actions affect others. You fear that you are not doing enough, but to everyone else, you are!",
+            },
+  'nerd': {
+            title: 'Open-minded and big-hearted',
+            contents: "You are the Harry Potter Pup. The pup chosen to right the wrongs in the world and bring harmony back to Middle Earth. You enjoy free-range chicken, reading historical biographies and retaking the Myers Briggs Personality test to reaffirm that your personality type is as rare as a snowflake landing on a jaguar's backside. You have magic powers, but I would highly recommend not putting that on your resume, since (1) people might not agree and (2) you are a dog and seeking traditional employment might already be difficult."
+          },
+  'shy': {
+            title: 'Sometimes shy, but secretly adventurous',
+            contents: "You are a very thoughtful person, both when it comes to thinking of others and thinking about ideas. You enjoy nature, particularly the way light filters through leaves. In a group, you are almost never the first person to talk, but when you do, people always want you to keep talking. You value the future over the past. You dislike puns and enjoy handwritten notes; handwritten puns are acceptable. It is important to you to try something new each week. Though you are afraid of heights, you sometimes google pictures of tall cliffs just to scare yourself a little. You're not afraid to take on a challenge, and that is very cool.",
+          },
+  'sleeping': {
+                title: 'Independent and free',
+                contents: "You are very energized about the many things you find important, and this picture was stolen during a brief moment of relaxation. The ability to take risks is important to you, and you get restless when you feel like you haven't changed in a while. You are good at articulating your ideas. Clocks are one of your least favorite things. You always get separated from your group when exploring a new place, so thank goodness you have an excellent sense of direction. The answer to your security question is always your library card number (\"What is the name of your elementary school?\" \"0074117796323\"). You have a hidden talent that few know about, but since I already gave away your security question, I'll keep that a secret.",
+              },
+  'sleepy': {
+              title: 'Stronger than you know.',
+              contents: "You call it \"meditation\", but it's more like \"closing your eyes between hitting snooze\". You call it \"lifting weights\", but it's more like \"a good excuse to drink Gatorade\". You describe yourself as \"C-type\" -- \"C for chill\". Your jokes are often well timed and enjoyed by all. You transcend difficulties in life, by smoothly flowing around them. You eat fortune cookies, but never look at the fortune because of your deep internal confidence. Napping is a skill you practice often, but man oh man, once your internal engine gets going you focus 100%. Maybe even 110%. It's like a lot of focus. So much focus. Buckets of Focus. A whole Ford Focus. But right now, you're snoozing and that's cool. You'll solve P=NP a little later.",
+            }
+};
