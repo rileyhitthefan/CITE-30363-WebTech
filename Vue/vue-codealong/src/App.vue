d607486d87c85658bac126ff1ed5f44052baf62a
riley <riley.phan@tcu.edu>
2025-02-23 12:34:34 -0600

    Attribute bindings

---
+++ Vue/vue-codealong/src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <h1>{{ message }}</h1>
+  <img v-bind:src="imageUrl" alt="" />
+  <br />
+
+  <!-- shorthand for v-bind -->
+  <img :src="imageUrl" alt="" />
+  <br />
+
+  <button @click="changeImg">Change image</button>
+
+  <br />
+
+  <hr />
+
+  <input type="text" :value="defaultInputText" />
+
+  <hr />
+
+  <p :class="className">Harry Potter</p>
+
+  <!-- define a JS object in :class -->
+  <p :class="{ inactive: isInactive, center: isCenter }">
+    <!-- if you think embedding a JS object in HTML is verbose, you can choose to move the object to the script, 
+        give it a name, and only put the JS object name in :class -->
+    Harry Potter
+  </p>
+
+  <!-- define a JS array in :class -->
+  <p :class="['active', 'center']">Harry Potter</p>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+let message = 'Hello, v-bind!'
+// reactivity
+let imageUrl = ref('public/img/banner_1.jpg') 
+
+function changeImg() {
+  if (imageUrl.value === 'public/img/banner_1.jpg') {
+    imageUrl.value = 'public/img/banner_2.jpg'
+    return
+  }
+  else if (imageUrl.value === 'public/img/banner_2.jpg') {
+    imageUrl.value = 'public/img/banner_3.jpg'
+    return
+  }
+  else {
+    imageUrl.value = 'public/img/banner_1.jpg'
+    return
+  }
+}
+
+let defaultInputText = 'Write something here...'
+
+let className = ref('active')
+let isInactive = ref(true)
+let isCenter = ref(false)
+</script>
+
+<style scoped>
+img {
+  max-width: 300px;
+}
+
+.active {
+  color: green;
+}
+
+.inactive {
+  color: red;
+  text-decoration: line-through;
+}
+
+.center {
+  text-align: center;
+}
+</style>
