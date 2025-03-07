253698e7f4d593477160a63d81c71915a97e890c
riley <riley.phan@tcu.edu>
2025-03-07 14:25:12 -0600

    introduction vue components

---
+++ Vue/vue-components-intro/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <ButtonCounter></ButtonCounter>
+  <ButtonCounter></ButtonCounter>
+
+  <button-counter></button-counter>
+
+  <hr />
+
+  <div style="background-color: orange; padding: 5px">
+    This is the root component.
+    <ComponentA></ComponentA>
+    <ComponentA></ComponentA>
+  </div>
+</template>
+
+<script setup>
+  import ButtonCounter from './ButtonCounter.vue'
+  import ComponentA from '@/ComponentA.vue'
+</script>
+
+<style scoped></style>
