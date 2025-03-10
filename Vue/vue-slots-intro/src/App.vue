680737829ad8f75f79563800df9a6e0855b5f03f
riley <riley.phan@tcu.edu>
2025-03-09 20:41:08 -0500

    intro to vue slots

---
+++ Vue/vue-slots-intro/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <AlertBox>Invalid username or password. Please try again.</AlertBox>
+  <AlertBox></AlertBox>
+  <AlertBox>
+    <h1>The request timed out. Please refresh the page and try again.</h1>
+  </AlertBox>
+</template>
+
+<script setup>
+import AlertBox from './AlertBox.vue'
+</script>
+
+<style lang="scss" scoped></style>
